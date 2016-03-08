004fd0ddd76a84d71f1cabeff5d56d8bd312f6a5
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-03-08 16:37:13 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var $header = $('header');
 var $h1 = $('h1');
 var $main = $('main');
 var $h2 = $('h2');
-var $img = $('img')
+var $img = $('img');
 
 $header.addClass('masthead');
 
@@ -15,13 +15,15 @@ $('main p:first-child').addClass('intro');
 
 $h2.addClass('slug-head');
 
-$('main ul').addClass('slug-list')
+$('main ul').addClass('slug-list');
 
-$img.addClass('slug-img')
+$img.addClass('slug-img');
 
 $('ul li:first-child').addClass('slug-list-first');
 $('ul li:last-child').addClass('slug-list-last');
 
-$('main dl').addClass('classification')
+$('main dl').addClass('classification');
 
 $('dl dt').removeClass('big-label');
+
+$('ul li:last-child').show();
