1ac02c8229a451116fd0a65d1a02ba5f23d956f5
goug0035 <marygough@Marys-MacBook-Pro.local>
2016-03-08 16:24:21 -0500

    commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var $header = $('header');
+var $h1 = $('h1');
+var $main = $('main');
+var $h2 = $('h2');
+var $img = $('img')
+
+$header.addClass('masthead');
+
+$h1.addClass('logo');
+$h1.removeClass('big-heading');
+
+$main.addClass('wrapper');
+
+$('main p:first-child').addClass('intro');
+
+$h2.addClass('slug-head');
+
+$('main ul').addClass('slug-list')
+
+$img.addClass('slug-img')
+
+$('ul li:first-child').addClass('slug-list-first');
+$('ul li:last-child').addClass('slug-list-last');
+
+$('main dl').addClass('classification')
+
+$('dl dt').removeClass('big-label');
